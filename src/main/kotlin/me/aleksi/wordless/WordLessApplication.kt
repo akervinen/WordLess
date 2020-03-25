0ac124e84e790097e6b42090a0bb2fcb56e5d16b
Aleksi Kervinen <me@aleksi.me>
2020-03-25 21:18:43 +0200

    initial commit with Spring Boot base

---
+++ src/main/kotlin/me/aleksi/wordless/WordLessApplication.kt
@@ -0,0 +1,11 @@
+package me.aleksi.wordless
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WordLessApplication
+
+fun main(args: Array<String>) {
+	runApplication<WordLessApplication>(*args)
+}
