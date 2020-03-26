464c0a20d063f55509daa07aa5a50a523022ad14
Aleksi Kervinen <me@aleksi.me>
2020-03-26 21:17:10 +0200

    add post listing, single post view

--- src/main/javascript/src/App.jsx
+++ src/main/javascript/src/App.jsx
@@ -1,28 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment, useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Switch, useParams} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  fetch('/api/test').then(res => {
-    console.log('hello' + res.status);
-  });
+function PostHeader(props) {
+  const {post} = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>
+        <a href={`/posts/${post.id}/${post.slug}`}>{post.title}</a>
+      </h1>
+      <p className="meta">
+        <time dateTime={post.postedTime}>{new Date(post.postedTime).toLocaleString()}</time>
+      </p>
+    </header>);
+}
+
+function PostSummary(props) {
+  const {post} = props;
+  return (<article>
+    <PostHeader post={post}/>
+
+    <div className="summary">
+      {post.summary}
     </div>
+
+    <footer>
+      <a href={`/posts/${post.id}/${post.slug}`}>Read More...</a>
+    </footer>
+  </article>);
+}
+
+function Post() {
+  const {id} = useParams();
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    (async function fetchData() {
+      const result = await fetch(`/api/posts/${id}`);
+      const json = await result.json();
+      setPost(json);
+    })();
+  }, []);
+
+  return (
+    <article>
+      <PostHeader post={post}/>
+
+      <div className="content">
+        {post.content}
+      </div>
+    </article>);
+}
+
+function PostList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    (async function fetchData() {
+      const result = await fetch('/api/posts');
+      const json = await result.json();
+      setPosts(json);
+    })();
+  }, []);
+
+  return (
+    <Fragment>
+      {posts.map(item => (
+        <PostSummary post={item}/>)
+      )}
+    </Fragment>);
+}
+
+function App() {
+  return (
+    <Router>
+      <header>
+        <h1>Spaghetti Forever</h1>
+      </header>
+
+      <main>
+        <Switch>
+          <Route path="/posts/:id" children={<Post/>}/>
+          <Route path="/">
+            <PostList/>
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
