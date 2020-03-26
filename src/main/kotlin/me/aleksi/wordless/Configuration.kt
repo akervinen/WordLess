fc29caf76ee6992a7f77c2ab118b4dde97105093
Aleksi Kervinen <me@aleksi.me>
2020-03-26 22:51:59 +0200

    fix navigating from outside SPA

--- src/main/kotlin/me/aleksi/wordless/Configuration.kt
+++ src/main/kotlin/me/aleksi/wordless/Configuration.kt
@@ -1,8 +1,12 @@
 package me.aleksi.wordless
 
 import org.jdbi.v3.spring4.JdbiFactoryBean
+import org.springframework.boot.web.server.ConfigurableWebServerFactory
+import org.springframework.boot.web.server.ErrorPage
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy
 import javax.sql.DataSource
 
@@ -13,5 +17,12 @@ class Configuration {
         val jfb = JdbiFactoryBean(TransactionAwareDataSourceProxy(dataSource))
         jfb.setAutoInstallPlugins(true)
         return jfb
+    }
+
+    @Bean
+    fun notFoundCustomizer(): WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+        return WebServerFactoryCustomizer<ConfigurableWebServerFactory> { factory ->
+            factory?.addErrorPages(ErrorPage(HttpStatus.NOT_FOUND, "/"))
+        }
     }
 }
